78204dc5fb2e1b6ca1db0671aac92e792efd2be5
Maryucha_Mariani <https://Maryucha_Mariani@bitbucket.org/Maryucha_Mariani/appsensores.git>
2021-08-12 15:16:24 -0300

    criando um localStorage

--- app.js
+++ app.js
@@ -1,19 +1,7 @@
 'use strict';
 
-let bancoDeTarefas = [
-    {
-        'tarefa':'estudar javaScript',
-        'status': ''
-    },
-    {
-        'tarefa':'estudar Angular',
-        'status': 'checked'
-    },
-    {
-        'tarefa':'Netflix Outlander',
-        'status': ''
-    }
-]
+const getBancoLocalStorage = () => JSON.parse(localStorage.getItem('todoList')) ?? [];
+const setBancoLocalStorage = (bancoDeTarefas) => localStorage.setItem('todoList',JSON.stringify(bancoDeTarefas));
 
 const criarItem = (tarefa, status, indice) => {
     const item = document.createElement('label');
@@ -26,9 +14,11 @@ const criarItem = (tarefa, status, indice) => {
         document.getElementById('todoList').appendChild(item);
 }
 
+
 const atualizaTela = () => {
     limparTarefas();
-   bancoDeTarefas.forEach((item, indice) => criarItem(item.tarefa, item.status, indice));
+    const bancoDeTarefas = getBancoLocalStorage();
+        bancoDeTarefas.forEach((item, indice) => criarItem(item.tarefa, item.status, indice));
 } 
 
 const limparTarefas = () => {
@@ -42,7 +32,9 @@ const criarNovaTarefa = (evento) => {
     const tecla = evento.key;
     const tituloTarefa = evento.target.value;
     if(tecla === 'Enter'){
+        const bancoDeTarefas = getBancoLocalStorage();
         bancoDeTarefas.push({'tarefa': tituloTarefa, 'status': ''});
+        setBancoLocalStorage(bancoDeTarefas);
         atualizaTela();
         //limpar a caixa de texto
         evento.target.value = '';
@@ -61,13 +53,22 @@ const clickItem = (evento) => {
 }
 
 const atualizarItem = (indice) => {
-    //operador ternário
+
+    const bancoDeTarefas = getBancoLocalStorage();
+    
     bancoDeTarefas[indice].status = bancoDeTarefas[indice].status === '' ? 'checked' : '';
+    
+    setBancoLocalStorage(bancoDeTarefas);
+    
     atualizaTela();
 }
 
 const removerItem = (indice) => {
+    const bancoDeTarefas = getBancoLocalStorage();
+
     bancoDeTarefas.splice(indice, 1);
+    setBancoLocalStorage(bancoDeTarefas);
+
     atualizaTela();
 }
 
